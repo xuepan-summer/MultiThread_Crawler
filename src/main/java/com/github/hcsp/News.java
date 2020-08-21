f608128011500cf7ccef485c424d553f51471723
summer <summer_xue.zz@foxmail.com>
2020-08-21 10:16:22 +0800

    新增News类

---
+++ src/main/java/com/github/hcsp/News.java
@@ -0,0 +1,40 @@
+package com.github.hcsp;
+
+public class News {
+    private Integer id;
+    private String title;
+    private String text;
+    private String url;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
