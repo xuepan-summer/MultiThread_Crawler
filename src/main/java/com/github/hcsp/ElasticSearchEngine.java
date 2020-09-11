def5cb2577d63e35f3cfad689a727ffad5ba35e2
summer <summer_xue.zz@foxmail.com>
2020-09-11 21:01:41 +0800

    构建ElasticSearch搜索引擎
    maven引入，docker安装ElasticSearch
    本机作为client，从DB取数据，向ES发送请求，写入数据
    构造ES搜索引擎，终端输入关键字，打印出相关新闻

---
+++ src/main/java/com/github/hcsp/ElasticSearchEngine.java
@@ -0,0 +1,34 @@
+package com.github.hcsp;
+
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.MultiMatchQueryBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class ElasticSearchEngine {
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            System.out.println("Please input the keyword:");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+            String keyWord = bufferedReader.readLine();
+
+            try (RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")))) {
+                SearchRequest searchRequest = new SearchRequest("news");
+                searchRequest.source(new SearchSourceBuilder().query(new MultiMatchQueryBuilder(keyWord, "title", "content")));
+//                searchRequest.source().query(new MultiMatchQueryBuilder(keyWord, "title", "content"));
+                SearchResponse result = client.search(searchRequest, RequestOptions.DEFAULT);
+                result.getHits().forEach(hit -> System.out.println(hit.getSourceAsMap()));
+            }
+        }
+    }
+}
